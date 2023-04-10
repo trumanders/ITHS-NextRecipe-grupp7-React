2c0770a599e6cd841662377e3fc97a6437924909
Mariette Boström <mariette.bostrom@iths.se>
2023-04-10 10:18:02 +0200

    Layout and pagecontainer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Layout from './Layout'
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
-    </div>
+    
+      <Layout/>
+    
   )
 }
 
---
+++ src/Layout.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import PageContainer from './PageContainer';
+export default Layout
+
+function Layout() { 
+  return (    
+    <>
+    
+    {/* Här ska stoppas navbaren in */}
+
+    <PageContainer/>
+
+    </>
+        
+        
+  )
+}
