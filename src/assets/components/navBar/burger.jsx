9fbbd9a1e366f3cc61d48945afcf5b6e693b323d
Stefan Lundkvist <varulvsnatt@hotmail.com>
2023-04-10 18:57:36 +0200

    Basic style done. Fits mobile and Desktop.

---
+++ src/assets/components/navBar/burger.jsx
@@ -0,0 +1,13 @@
+import './burger.css'
+
+function Burger(){
+    return(
+        <div className="burger_menu">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
+
+export {Burger}
