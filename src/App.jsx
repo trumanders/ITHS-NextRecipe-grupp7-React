81e9b22e61e916a1f05e32407691d4babe3a2ba3
Shakiba Kouchakpour <shakiba.kouchakpour@iths.se>
2023-04-15 12:39:13 +0200

    Integrating RecipeCard into RecipeResresentation using utils.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import Layout from "./Layout";
 import "bootstrap/dist/css/bootstrap.min.css";
+import RecipeRepresentation from "./assets/RecipeRepresentation";
+
 function App() {
   return (
     <div>
       <Layout />;
+      <RecipeRepresentation/>;
     </div>
   );
 }
---
+++ src/assets/RecipeRepresentation.jsx
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react'
+import RecipeCard from "./RecipeCard";
+import { getPopularRecipes } from '../utils';
+
+export default function RecipeRepresentation(){
+    const [recipes, setRecipes]= useState([]);
+
+    useEffect(()=> { 
+    async function fetchData(){
+        const response= await getPopularRecipes();
+        setRecipes(response)
+    }
+    fetchData();
+    },[])
+
+    return(
+    <><h1>Popular Recipes</h1>
+    <div className='recipeRepresentation'>
+    {recipes.map(popularRecipe=> 
+    <RecipeCard {...popularRecipe} key={popularRecipe.id}/>)}
+    </div>
+    </>)
+}
