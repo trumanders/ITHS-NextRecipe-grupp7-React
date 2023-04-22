9c07116728f2225ddca10c1efaa11f685cbe5447
magnus <magnus.jacobson@hotmail.com>
2023-04-22 13:15:29 +0200

    faulty merge-repair

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 import PageContainer from "./PageContainer";
 export default Layout;
 import { NavBar } from "./assets/components/navBar/NavBar.jsx";
-import Recipe from "./assets/components/recipe-info/Recipe";
+import Search from "./assets/components/Searchbar/Search.jsx";
 
 
 
 function Layout() {
   return (
-    <>
+    <>      
+      <NavBar />
+      <Search />   
+      <PageContainer />
+      {/* <Footer></Footer> */}
     </>
   )
 }
