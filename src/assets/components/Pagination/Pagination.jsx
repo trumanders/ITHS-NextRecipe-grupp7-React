94bef0b586c85c6ba24ad71514f7f9ebd495309b
Shakiba Kouchakpour <shakiba.kouchakpour@iths.se>
2023-04-22 12:30:59 +0200

    Added pagonation / Changes in recipeRepresentation / Changes in utils : total numbers of fetched recepies = 100

---
+++ src/assets/components/Pagination/Pagination.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+//Taking props in from th eparent component
+export default function Pagination({
+    recipesPerPage,
+    totalRecipes,
+    currentPage,
+    onPageChange
+}){
+    //Calculating the total amount of pages, using Math.ceil to round up the resulted number 
+    const totalPages = Math.ceil(totalRecipes / recipesPerPage); 
+      
+    //Current page is a state in parent component and is sent to here as a prop 
+    function handleNextPage(){
+        onPageChange(currentPage + 1);
+    }
+  
+    function handlePreviousPage(){
+        onPageChange(currentPage -1);
+    }
+
+    // Checking if the page is not 1, then we use Previous button 
+    // Also checking if the page is smaller than the total number of pages, then we can use Next button 
+    return(<> 
+       <div className="pagination">
+          {currentPage!==1 &&(
+            <button className="pageBtn" onClick={handlePreviousPage}>Previous</button>
+          )}
+          {currentPage!== totalPages && (
+            <button  className="pageBtn" onClick={handleNextPage}>Next</button>
+          )}
+      </div>
+    </>)
+}
--- src/assets/components/RecipeRepresentation/RecipeRepresentation.jsx
+++ src/assets/components/RecipeRepresentation/RecipeRepresentation.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import RecipeCard from "../RecipeCard/RecipeCard";
 import { getPopularRecipes } from "../../../utils";
+import { func } from "prop-types";
+import Pagination from "../Pagination/Pagination";
+
 
 export default function RecipeRepresentation() {
   const [recipes, setRecipes] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
@@ -13,13 +17,36 @@ export default function RecipeRepresentation() {
     fetchData();
   }, []);
 
+  //Using Slice method to cut out one specific part of the recipes array which comes in as state
+  // To use Slice method we need a starting point and a finishing point (The index of the first recipe in each page and the last one)
+  // I start by finding the index of the last recipe by multiplying the value of the currentPage into the value of the recipesPerPage
+  // Then I calculate theh index of the first recipe per page by subtracting recipesPerPage from the indexOfLastRecipeInCurrentPage 
+  const recipesPerPage = 8;
+  const indexOfLastRecipeInCurrentPage = currentPage * recipesPerPage;
+  const indexOfFirstRecipeInCurrentPage = indexOfLastRecipeInCurrentPage - recipesPerPage;
+  const currentRecipes = recipes.slice(indexOfFirstRecipeInCurrentPage, indexOfLastRecipeInCurrentPage);   
+
+  // seting the current state by using this function and passing in the page as prop
+  function handleOnPageChange(page){
+    setCurrentPage(page);
+  }
+
+
+  // mapping through the sliced array (currentRecipes)
+  // rendering Pagination and sending the necessary props to Pagination component
   return (
     <>
       <h1>Popular Recipes</h1>
       <div className="recipeRepresentation">
-        {recipes.map((popularRecipe) => (
+        {currentRecipes.map((popularRecipe) => (
           <RecipeCard {...popularRecipe} key={popularRecipe.id} />
         ))}
+      </div>
+      <div className="pagination">
+        <Pagination recipesPerPage={recipesPerPage}
+        totalRecipes={recipes.length}
+        currentPage={currentPage}
+        onPageChange={handleOnPageChange}></Pagination>
       </div>
     </>
   );
