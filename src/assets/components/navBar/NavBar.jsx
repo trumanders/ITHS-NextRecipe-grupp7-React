e02e80134d5cbfa0a6cad06a9b7f31c170316d0a
magnus <magnus.jacobson@hotmail.com>
2023-04-22 13:06:43 +0200

    faulty merge-repair

--- src/assets/components/navBar/NavBar.jsx
+++ src/assets/components/navBar/NavBar.jsx
@@ -1,28 +1,53 @@
-import './NavBar.css'
-import './NavFunc'
-import {Burger} from './burger.jsx'
-import {Link} from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./NavBar.css";
+import "./NavFunc";
+import { Burger } from "./burger.jsx";
+import { Link } from "react-router-dom";
 
 function NavBar(){
-    return (
+    const [visible, setVisible] = useState(false)
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 730);
+
+    const updateMedia = () => {
+        setIsMobile(window.innerWidth < 730)
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    }, [isMobile]);
+
+  return (
     <div className="NavBar">
         <a href="./"><img src="logotype.png" className="Logotype" alt="Logotype"/></a>
         <div className="NaviIntro">
-            <Burger></Burger>
-            <p>Website Name</p>
+            <button onClick={() => setVisible(!visible)}><Burger></Burger></button>
+            <p>Nail Soup</p>
             <div className="searchIcon">
-                <img src="src\assets\components\navBar\pngegg.png" alt="Search recipe" />
+                <img src="src/assets/components/navBar/pngegg.png" alt="Search recipe" />
             </div>
         </div>
+            {!isMobile || visible ? (
         <ul>
-            <li className="navOpt"><p>Home</p></li>
-            <li className="navOpt"><p>Adv. Search</p></li>
-            <li className="navOpt"><Link to="/about">About</Link></li>
-            <li className="navOpt"><Link to="/contact">Contact</Link></li>
-            
+          <li className="navOpt">
+            <Link to="">
+              <p>Home</p>
+            </Link>
+          </li>
+          <li className="navOpt">
+            <Link to="/about">
+              <p>About</p>
+            </Link>
+          </li>
+          <li className="navOpt">
+            <Link to="/contact">
+              <p>Contact</p>
+            </Link>
+          </li>
         </ul>
+        ) : (<></>)}
     </div>
-    )
+  );
 }
 
 export {NavBar};
