cbd0966bf0b22b7885127abe91c88c1879ec582b
Stefan Lundkvist <varulvsnatt@hotmail.com>
2023-04-24 09:40:44 +0200

    Updating CSS

--- src/PageContainer.jsx
+++ src/PageContainer.jsx
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import AboutUs from "./assets/components/about/AboutUs";
 import { ContactUs } from "./assets/components/contact/ContactForm";
 import RecipeRepresentation from "./assets/components/RecipeRepresentation/RecipeRepresentation";
+import Search from "./assets/components/Searchbar/Search.jsx";
 
 import {
   Routes,
@@ -14,14 +15,15 @@ import {
 } from "react-router-dom";
 function PageContainer() {
   return (
-    <Container>
+    <div className="wrapper">
+      <Search />
       {/* SEARCH-BAR HÄR? Förutom när about eller contact visas? */}
       <Routes>
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/" element={<RecipeRepresentation />} />
       </Routes>
-    </Container>
+    </div>
   );
 }
 export default PageContainer;
