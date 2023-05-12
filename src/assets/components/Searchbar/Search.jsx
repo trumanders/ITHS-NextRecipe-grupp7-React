9a94f4b8d103001418cdcd1dd48fb7b72aac3a65
Anders Johansson <trumanders@gmail.com>
2023-05-12 11:11:43 +0200

    Ingrenser fastnar som de ska efter en sökning.

--- src/assets/components/Searchbar/Search.jsx
+++ src/assets/components/Searchbar/Search.jsx
@@ -1,35 +1,36 @@
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import React, {useEffect, useState} from 'react';
-import './Search.css';
-import Button from 'react-bootstrap/Button';
-import CloseButton from 'react-bootstrap/CloseButton';
-import { useClickStore } from '../../hooks/useClickStore';
-import { useSearchStringStore } from '../../hooks/useSearchStringStore';
-import { shallow } from 'zustand/shallow';
-import CustomAccordion from './Accordion'
-
-
-
-
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import React, { useEffect, useState } from "react";
+import "./Search.css";
+import Button from "react-bootstrap/Button";
+import CloseButton from "react-bootstrap/CloseButton";
+import { useClickStore } from "../../hooks/useClickStore";
+import { useSearchStringStore } from "../../hooks/useSearchStringStore";
+import { shallow } from "zustand/shallow";
+import CustomAccordion from "./Accordion";
 
 function Search() {
   const [input, setInput] = useState("");
-  const [listInputs, setlistInputs] = useState([]);
-  const [recipeSearch, setrecipeSearch] = useState("");
-  const [listDiet, setlistDiet] = useState([]);
-  const [listType, setlistType] = useState("");
-  const [listIntolerances, setlistIntolerances] = useState([]);
+
   const [searchString, setSearchString] = useSearchStringStore(
     (state) => [state.searchString, state.setSearchString],
     shallow
   );
+
+  const [listInputs, setlistInputs] = useState(
+    convertToArray(searchString.ingredients)
+  );
+  const [recipeSearch, setrecipeSearch] = useState("");
+  const [listDiet, setlistDiet] = useState([]);
+  const [listType, setlistType] = useState("");
+  const [listIntolerances, setlistIntolerances] = useState([]);
+
   const [isClicked, setIsClicked] = useClickStore(
     (state) => [state.isClicked, state.setIsClicked],
     shallow
   );
-  const [alertMsgRecipe, setAlertMsgRecipe] = useState("")
-  const [alertMsgIngredient, setAlertMsgIngredient] = useState("")
+  const [alertMsgRecipe, setAlertMsgRecipe] = useState("");
+  const [alertMsgIngredient, setAlertMsgIngredient] = useState("");
   const [isMobile, setMobile] = useState(window.innerWidth < 730);
 
   const updateMedia = () => {
@@ -41,78 +42,83 @@ function Search() {
     return () => window.removeEventListener("resize", updateMedia);
   }, [isMobile]);
 
+  // Gör om searchString till en array. Om arrayen innehåller en tom sträng, returnera en tom array.
+  function convertToArray(str) {
+    const ingredientArr = str.split(",").map((item) => item);
+    return ingredientArr.length === 1 && ingredientArr[0] === ""
+      ? []
+      : ingredientArr;
+  }
+
   // Funktion för addera ingridienser till lista med felhantering mot dubbla inputs
-    const handleSubmit=(event)=> {
-      event.preventDefault();
-      const item = input
-      
-      if(listInputs.includes(item))
-      {
-        setAlertMsgIngredient("Ingredient already added.")
-        setInput("")
-        return;
-      }
-      if(input)
-      {
-        setlistInputs((ls) =>[...ls, item])
-        setInput("")
-        setAlertMsgIngredient("")
-      }
-      else{
-        setAlertMsgIngredient("No ingredient added")
-      }
-      
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const item = input;
+
+    if (listInputs.includes(item)) {
+      setAlertMsgIngredient("Ingredient already added.");
+      setInput("");
+      return;
     }
-  
-
-    // Funktion för att ta bort ingridienserna från listan. 
-    const deleteInput = value => {
-      setlistInputs(oldValues => {
-        
-        return oldValues.filter(item => item !== value)
-        
-      })
+    if (input) {
+      setlistInputs((ls) => [...ls, item]);
+      setInput("");
+      setAlertMsgIngredient("");
+    } else {
+      setAlertMsgIngredient("No ingredient added");
     }
+  };
 
-    // Funktion för att ta emot flera värden från checkboxar, om checkad läggs värdet till i listan, om inte så tas värdet bort.
-    //  const handleDietbox =(event) =>{
-      
-    //    const{value, checked} = event.target
-       
-    //    if(checked)
-    //    {
-    //     setlistDiet(diets => [...diets,value])
-    //    }
-    //    else(
-    //     setlistDiet(diets => {
-    //       return [...diets.filter(dietValue => dietValue !== value)]
-    //     })
-    //    )
+  // Funktion för att ta bort ingridienserna från listan.
+  const deleteInput = (value) => {
+    setlistInputs((oldValues) => {
+      return oldValues.filter((item) => item !== value);
+    });
+  };
 
-    //   }
-      // Funktion för att ta emot flera värden från checkboxar, om checkad läggs värdet till i listan, om inte så tas värdet bort.
-      // const handleIntolerances =(event) => {
-      //   const{value, checked} = event.target
-       
-      //  if(checked)
-      //  {
-      //   setlistIntolerances(intolerances => [...intolerances,value])
-      //  }
-      //  else(
-      //   setlistIntolerances(diets => {
-      //     return [...diets.filter(intolerances => intolerances !== value)]
-      //   })
-      //  )
+  // Funktion för att ta emot flera värden från checkboxar, om checkad läggs värdet till i listan, om inte så tas värdet bort.
+  //  const handleDietbox =(event) =>{
 
-      // }
-     
-      // Gör om värden till strängar, som sedan kan skickas till searchStore och som sedan kanbehandlas i URL:en. Även felhantering om det ej finns ingridenser från input.
-      const sendIngridients = () => {
-      
-      
-        setSearchString({ingredients: listInputs.toString(), type: listType, intolerances: listIntolerances.toString(), diet: listDiet.toString(), call: "getIngredient"})
-        setIsClicked()
-      }
+  //    const{value, checked} = event.target
+
+  //    if(checked)
+  //    {
+  //     setlistDiet(diets => [...diets,value])
+  //    }
+  //    else(
+  //     setlistDiet(diets => {
+  //       return [...diets.filter(dietValue => dietValue !== value)]
+  //     })
+  //    )
+
+  //   }
+  // Funktion för att ta emot flera värden från checkboxar, om checkad läggs värdet till i listan, om inte så tas värdet bort.
+  // const handleIntolerances =(event) => {
+  //   const{value, checked} = event.target
+
+  //  if(checked)
+  //  {
+  //   setlistIntolerances(intolerances => [...intolerances,value])
+  //  }
+  //  else(
+  //   setlistIntolerances(diets => {
+  //     return [...diets.filter(intolerances => intolerances !== value)]
+  //   })
+  //  )
+
+  // }
+
+  // Gör om värden till strängar, som sedan kan skickas till searchStore och som sedan kanbehandlas i URL:en. Även felhantering om det ej finns ingridenser från input.
+  const sendIngridients = () => {
+    setSearchString({
+      ingredients: listInputs.toString(),
+      type: listType,
+      intolerances: listIntolerances.toString(),
+      diet: listDiet.toString(),
+      call: "getIngredient",
+    });
+    setIsClicked();
+  };
 
   // Gör om värden till strängar, som sedan kan skickas till searchStore och som sedan kan behandlas i URL:en. Om sökinput ej finns, så får man felmeddelande
   const sendRecipe = (event) => {
@@ -125,42 +131,68 @@ function Search() {
     }
     setSearchString({ ingredients: recipeSearch, call: "getRecipeSearch" });
     setIsClicked();
-    setAlertMsgRecipe("")
-    setrecipeSearch("")
+    setAlertMsgRecipe("");
+    setrecipeSearch("");
   };
 
-      // Gör om värden till strängar, som sedan kan skickas till searchStore och som sedan kan behandlas i URL:en.
-      const sendRandom = () => {
-        
-        const valuesRandom = [listDiet, listType, listIntolerances].toString()
-          setSearchString({ingredients: valuesRandom, call: "getRandom"})
-          setIsClicked()
-      }
+  // Gör om värden till strängar, som sedan kan skickas till searchStore och som sedan kan behandlas i URL:en.
+  const sendRandom = () => {
+    const valuesRandom = [listDiet, listType, listIntolerances].toString();
+    setSearchString({ ingredients: valuesRandom, call: "getRandom" });
+    setIsClicked();
+  };
 
-      const setNull =() =>{
-        setlistDiet([])
-        setlistType("")
-        setlistIntolerances([])
-      }
+  const setNull = () => {
+    setlistDiet([]);
+    setlistType("");
+    setlistIntolerances([]);
+  };
 
   return (
     <div className="searchpadding">
-    <Tabs
-      id="searchtabs"
-      className="justify-content-center"
-      onClick={setNull}
-    >
-      <Tab eventKey="take-what-you-have" title="Take what you have">
-        <p className="textpadding">Here you will find recipes based on what ingridients you have at home.</p>
+      <Tabs
+        id="searchtabs"
+        className="justify-content-center"
+        onClick={setNull}
+      >
+        <Tab eventKey="take-what-you-have" title="Take what you have">
+          <p className="textpadding">
+            Here you will find recipes based on what ingridients you have at
+            home.
+          </p>
 
-        
+          <form onSubmit={handleSubmit} className="search-form">
+            <div
+              className="searchbar"
+              style={
+                alertMsgIngredient !== ""
+                  ? { padding: 0 }
+                  : { paddingBottom: 23 }
+              }
+            >
+              <input
+                type="text"
+                placeholder="Add your ingridients"
+                value={input}
+                name="tab1"
+                className={
+                  alertMsgIngredient !== ""
+                    ? "search-text-alert"
+                    : "search-text"
+                }
+                onChange={(event) => setInput(event.target.value)}
+              />
+              <Button
+                className="addBtn"
+                variant="dark"
+                type="button"
+                onClick={handleSubmit}
+              >
+                Add
+              </Button>
+              <div className="alertOutput">{alertMsgIngredient}</div>
+            </div>
 
-        <form onSubmit={handleSubmit} className="search-form">
-        <div className="searchbar" style={ alertMsgIngredient !== "" ? {padding: 0} : {paddingBottom: 23}}>
-            <input type="text" placeholder="Add your ingridients" value={input} name="tab1" className={alertMsgIngredient !== "" ? "search-text-alert" : "search-text"} onChange={(event)=>setInput(event.target.value)}/>
-            <Button className='addBtn' variant="dark" type="button" onClick={handleSubmit}>Add</Button>
-            <div className="alertOutput">{alertMsgIngredient}</div>
-          </div>
             <ul id="itemContainer">
               {listInputs.map((item) => {
                 return (
@@ -180,29 +212,43 @@ function Search() {
             <Button onClick={sendIngridients} variant="outline-dark">
               Search
             </Button>
-            
-            <CustomAccordion listType={listType} setlistType={setlistType} listDiet={listDiet} setlistDiet={setlistDiet} listIntolerances={listIntolerances} setlistIntolerances={setlistIntolerances} />
-        </form>
-  
-          
-        
-        
-      </Tab>
-      <Tab eventKey="home" title="Recipes">
-      <p className="textpadding">Search recipes</p>
-      
-        <form onSubmit={sendRecipe} className="search">
-        <div className="searchbar" style={ alertMsgRecipe !== "" ? {padding: 0} : {paddingBottom: 23}}>
-        <input type="text" placeholder="Recipe" value={recipeSearch} name="tab2" 
-          className={alertMsgRecipe !== "" ? "search-text-alert" : "search-text"} onChange={(event)=>setrecipeSearch(event.target.value)} />
-        <div className="alertOutput">{alertMsgRecipe}</div>
+            <CustomAccordion
+              listType={listType}
+              setlistType={setlistType}
+              listDiet={listDiet}
+              setlistDiet={setlistDiet}
+              listIntolerances={listIntolerances}
+              setlistIntolerances={setlistIntolerances}
+            />
+          </form>
+        </Tab>
+        <Tab eventKey="home" title="Recipes">
+          <p className="textpadding">Search recipes</p>
+
+          <form onSubmit={sendRecipe} className="search">
+            <div
+              className="searchbar"
+              style={
+                alertMsgRecipe !== "" ? { padding: 0 } : { paddingBottom: 23 }
+              }
+            >
+              <input
+                type="text"
+                placeholder="Recipe"
+                value={recipeSearch}
+                name="tab2"
+                className={
+                  alertMsgRecipe !== "" ? "search-text-alert" : "search-text"
+                }
+                onChange={(event) => setrecipeSearch(event.target.value)}
+              />
+              <div className="alertOutput">{alertMsgRecipe}</div>
             </div>
-            <Button variant="outline-dark" type="Button" onClick={sendRecipe}>Search</Button>
-            
- 
-        </form>
-      
-      </Tab>
+            <Button variant="outline-dark" type="Button" onClick={sendRecipe}>
+              Search
+            </Button>
+          </form>
+        </Tab>
 
         <Tab eventKey="contact" title="Random recipe">
           <p className="textpadding">
@@ -213,13 +259,18 @@ function Search() {
             Go!
           </Button>
 
-        <CustomAccordion listType={listType} setlistType={setlistType} listDiet={listDiet} setlistDiet={setlistDiet} listIntolerances={listIntolerances} setlistIntolerances={setlistIntolerances} />
-       
-      </Tab>
-    </Tabs>
+          <CustomAccordion
+            listType={listType}
+            setlistType={setlistType}
+            listDiet={listDiet}
+            setlistDiet={setlistDiet}
+            listIntolerances={listIntolerances}
+            setlistIntolerances={setlistIntolerances}
+          />
+        </Tab>
+      </Tabs>
     </div>
   );
 }
 
 export default Search;
-
--- src/assets/pages/home/Home.jsx
+++ src/assets/pages/home/Home.jsx
@@ -1,93 +1,107 @@
-import {useState} from 'react'
-import { 
-    getPopularRecipes, 
-    filterRecipes,
-    getRecipeSearch,
-    getRandomRecipes, 
-    getRecipeByIngredients,
-} from '../../../utils'
-import Search from '../../components/Searchbar/Search'
-import RecipeRepresentation from '../../components/RecipeRepresentation/RecipeRepresentation'
-import { useSearchStringStore } from '../../hooks/useSearchStringStore'
-import { useClickStore } from '../../hooks/useClickStore'
-import { useLoaderData } from 'react-router-dom'
+import { useState } from "react";
+import {
+  getPopularRecipes,
+  filterRecipes,
+  getRecipeSearch,
+  getRandomRecipes,
+  getRecipeByIngredients,
+} from "../../../utils";
+import Search from "../../components/Searchbar/Search";
+import RecipeRepresentation from "../../components/RecipeRepresentation/RecipeRepresentation";
+import { useSearchStringStore } from "../../hooks/useSearchStringStore";
+import { useClickStore } from "../../hooks/useClickStore";
+import { useLoaderData } from "react-router-dom";
 
 //Laddar populära recept innan rendering
 export async function loader() {
-  const popularRecipes = await getPopularRecipes()
-  return {popularRecipes}
+  const popularRecipes = await getPopularRecipes();
+  return { popularRecipes };
 }
 
 export default function Home() {
-  const searchString = useSearchStringStore((state) => state.searchString)
-  const {popularRecipes} = useLoaderData()
-  const [isClicked, setIsClicked] = useClickStore(
-    (state) => [state.isClicked, state.setIsClicked])
-  const [prevClick, setPrevClick] = useState(0)
-  const [title, setTitle] = useState("Popular Recipes")
-  const [recipes, setRecipes] = useState(popularRecipes)
-  const [hasResults, setHasResults] = useState(true)
+  const searchString = useSearchStringStore((state) => state.searchString);
+  const { popularRecipes } = useLoaderData();
+  const [isClicked, setIsClicked] = useClickStore((state) => [
+    state.isClicked,
+    state.setIsClicked,
+  ]);
+  const [prevClick, setPrevClick] = useState(0);
+  const [title, setTitle] = useState("Popular Recipes");
+  const [recipes, setRecipes] = useState(popularRecipes);
+  const [hasResults, setHasResults] = useState(true);
 
   //När man trycker på ""search" kollar den vilken tab man gör det i och hämtar recept utifrån det.
-  if(isClicked > prevClick) {
+  if (isClicked > prevClick) {
     // setHasResults(true)
     switch (searchString.call) {
       case "getIngredient":
-        const fetchIngredient = async() => {
+        const fetchIngredient = async () => {
           //Om inga val är gjorda i 'advanced search' behöver inte två endpoints anropas.
-          const response = searchString.type === "" && searchString.intolerances === "" && searchString.diet === "" ? 
-          await getRecipeByIngredients(searchString.ingredients) : 
-          await filterRecipes(searchString.ingredients, searchString.type, searchString.intolerances, searchString.diet)
+          const response =
+            searchString.type === "" &&
+            searchString.intolerances === "" &&
+            searchString.diet === ""
+              ? await getRecipeByIngredients(searchString.ingredients)
+              : await filterRecipes(
+                  searchString.ingredients,
+                  searchString.type,
+                  searchString.intolerances,
+                  searchString.diet
+                );
 
           if (response.length < 1) {
-            setHasResults(false)
+            setHasResults(false);
           } else {
-          setRecipes(response)
-          setTitle(`Found ${response.length} recipes with ${searchString.ingredients}`)
+            setRecipes(response);
+            setTitle(
+              `Found ${response.length} recipes with ${searchString.ingredients}`
+            );
           }
-        }
-        fetchIngredient()
-        break
+        };
+        fetchIngredient();
+        break;
       case "getRecipeSearch":
-        const fetchFreeSearch = async() => {
-          const response = await getRecipeSearch(searchString.ingredients)
+        const fetchFreeSearch = async () => {
+          const response = await getRecipeSearch(searchString.ingredients);
           if (response.length < 1) {
-            setHasResults(false)
-            setRecipes([])
+            setHasResults(false);
+            setRecipes([]);
           } else {
-            setHasResults(true)
-          setRecipes(response)
-          setTitle(`Recipes with ${searchString.ingredients}`)
+            setHasResults(true);
+            setRecipes(response);
+            setTitle(`Recipes with ${searchString.ingredients}`);
           }
-        }
-        fetchFreeSearch()
-        break
+        };
+        fetchFreeSearch();
+        break;
       case "getRandom":
-        const fetchRandom = async() => {
-          const response = await getRandomRecipes(searchString.ingredients)
+        const fetchRandom = async () => {
+          const response = await getRandomRecipes(searchString.ingredients);
           if (response.length < 1) {
-            setHasResults(false)
+            setHasResults(false);
           } else {
-            setHasResults(true)
-          setRecipes(response)
-          setTitle("Random Recipes")
+            setHasResults(true);
+            setRecipes(response);
+            setTitle("Random Recipes");
           }
-        }
-        fetchRandom()
-        break
+        };
+        fetchRandom();
+        break;
     }
-    setPrevClick(prevClick + 1)
+    setPrevClick(prevClick + 1);
   }
 
-  return(
-  <>
-  <Search />
-  {!hasResults &&
-  <div className="noResult">
-  <h3>Sorry, no results found.</h3>
-  </div>}
-  {recipes.length > 0 && 
-  <RecipeRepresentation recipes={recipes} title={title} />}
-  </>
-  )
+  return (
+    <>
+      <Search />
+      {!hasResults && (
+        <div className="noResult">
+          <h3>Sorry, no results found.</h3>
+        </div>
+      )}
+      {recipes.length > 0 && (
+        <RecipeRepresentation recipes={recipes} title={title} />
+      )}
+    </>
+  );
 }
