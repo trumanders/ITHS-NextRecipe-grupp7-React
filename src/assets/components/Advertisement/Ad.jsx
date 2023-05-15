3f146c48e9ed0635fff80d25007d2b445af5bfcb
Anders Johansson <trumanders@gmail.com>
2023-05-15 11:47:01 +0200

    fixad margin på ad. Krockade med filters

--- src/assets/components/Advertisement/Ad.jsx
+++ src/assets/components/Advertisement/Ad.jsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 
 const adPhotos = [
-    'image1.jpg',
-    'image2.jpg',
-    'image3.jpg',
-    'image4.jpg',
-    'image5.jpg',
-    'image6.jpg',
-    'image7.jpg',
-    'image8.jpg',
-    'image9.jpg',
-  ];
+  "image1.jpg",
+  "image2.jpg",
+  "image3.jpg",
+  "image4.jpg",
+  "image5.jpg",
+  "image6.jpg",
+  "image7.jpg",
+  "image8.jpg",
+  "image9.jpg",
+];
 
-  const AdCarousel = () => {
-    return (
-      <Carousel>
-        {adPhotos.map((photo, index) => (
-          <Carousel.Item key={index}>
-            <img src={photo} alt={`Ad Photo ${index + 1}`} />
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    );
-  };
-  
-  export default AdCarousel;
-  
+const AdCarousel = () => {
+  return (
+    <Carousel style={{ margin: "25px" }}>
+      {adPhotos.map((photo, index) => (
+        <Carousel.Item key={index}>
+          <img src={photo} alt={`Ad Photo ${index + 1}`} className="img" />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default AdCarousel;
