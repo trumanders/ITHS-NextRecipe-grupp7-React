9459e38c6c5bdc987d501c7739809c71c1be2cb5
YakhoubSoumare <yakhoub.soumare1990@gmail.com>
2023-05-21 03:03:01 +0200

    so that caintainer stretches to bottom always

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,15 +2,19 @@ import { useState } from "react";
 import PageContainer from "./PageContainer";
 export default Layout;
 import { NavBar } from "./assets/components/navBar/NavBar.jsx";
+import './index.css'
 
 
 
 
 function Layout() {
   return (
-    <>      
+    <div className="layoutNew">      
       <NavBar /> 
-      <PageContainer />
-    </>
+      <div id="wrapper">
+        <PageContainer />
+      </div>
+      
+    </div>
   )
 }
