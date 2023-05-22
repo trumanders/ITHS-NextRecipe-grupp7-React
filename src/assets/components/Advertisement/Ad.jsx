f5710cf29189f131dd6465a4da39878230be5400
Stefan Lundkvist <varulvsnatt@hotmail.com>
2023-05-22 19:49:04 +0200

    Refactored, that's all.

--- src/assets/components/Advertisement/Ad.jsx
+++ src/assets/components/Advertisement/Ad.jsx
@@ -21,42 +21,39 @@ import admobile from '../../pictures/adNailSoupMobile.jpg'
  ]
 
 const AdCarousel = () => {
-  const[isMobile, setisMobile] = useState(window.innerWidth < 770)
+  const[isMobile, setisMobile] = useState(window.innerWidth < 760)
 
 
   const updateMedia = () => {
-    setisMobile(window.innerWidth < 770);
+    setisMobile(window.innerWidth < 760);
   };
 
-useEffect(() => {
-  window.addEventListener("resize", updateMedia);
-  return () => window.removeEventListener("resize", updateMedia)
-}, [isMobile]);
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia)
+  }, [isMobile]);
 
-if(isMobile)
-{
   return (
     <div className="adContainer">
-      <Carousel style={{ margin: "25px" }}>
-        {adMobilePhotos.map((photo, index) => (
-          <Carousel.Item key={index}>
-            <img src={photo} alt={`Ad Photo ${index + 1}`} className="img" />
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      {isMobile ? (
+        <Carousel style={{ margin: "25px" }}>
+          {adMobilePhotos.map((photo, index) => (
+            <Carousel.Item key={index}>
+              <img src={photo} alt={`Ad Photo ${index + 1}`} className="img" />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      ) : 
+      (
+        <Carousel style={{ margin: "25px" }}>
+          {adPhotos.map((photo, index) => (
+            <Carousel.Item key={index}>
+              <img src={photo} alt={`Ad Photo ${index + 1}`} className="img" />
+            </Carousel.Item>
+          ))}
+        </Carousel> 
+      )};
     </div>
   );
-      }
-  else{
-    return(
-    <Carousel style={{ margin: "25px" }}>
-      {adPhotos.map((photo, index) => (
-        <Carousel.Item key={index}>
-          <img src={photo} alt={`Ad Photo ${index + 1}`} className="img" />
-        </Carousel.Item>
-      ))}
-    </Carousel> 
-    );
-    }
 }
 export default AdCarousel;
