0c809f0fc460fcdc12408db0c3f53931578524ee
MaxWibITHS <max.wiberg@iths.se>
2023-05-22 13:38:36 +0200

    Added ad pictures, fixed responsetive design on ads

--- src/assets/components/Advertisement/Ad.jsx
+++ src/assets/components/Advertisement/Ad.jsx
@@ -1,32 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import './Ad.css'
 import obey from '../../pictures/AdObey.png'
 import korv from '../../pictures/AdKorv.png'
 import food from '../../pictures/AdFood.png'
+import hamb from '../../pictures/adNailSoup.jpg'
 
-const adPhotos = [
+// window.innerWidth < 720 ? setadList(adMobilePhotos) : setadList(adPhotos)
+ const adPhotos = [
+   hamb
+ ]
+
+ const adMobilePhotos = [
   obey,
-  korv,
-  food,
-  "image4.jpg",
-  "image5.jpg",
-  "image6.jpg",
-  "image7.jpg",
-  "image8.jpg",
-  "image9.jpg",
-];
+  korv
+ ]
 
 const AdCarousel = () => {
+  const[isMobile, setisMobile] = useState(window.innerWidth < 720)
+
+
+  const updateMedia = () => {
+    setisMobile(window.innerWidth < 730);
+  };
+
+useEffect(() => {
+  window.addEventListener("resize", updateMedia);
+  return () => window.removeEventListener("resize", updateMedia)
+}, [isMobile]);
+
+if(isMobile)
+{
   return (
     <Carousel style={{ margin: "25px" }}>
-      {adPhotos.map((photo, index) => (
+      {adMobilePhotos.map((photo, index) => (
         <Carousel.Item key={index}>
           <img src={photo} alt={`Ad Photo ${index + 1}`} className="img" />
         </Carousel.Item>
       ))}
     </Carousel>
   );
-};
+      }
+  else{
+    return(
+    <Carousel style={{ margin: "25px" }}>
+      {adPhotos.map((photo, index) => (
+        <Carousel.Item key={index}>
+          <img src={photo} alt={`Ad Photo ${index + 1}`} className="img" />
+        </Carousel.Item>
+      ))}
+    </Carousel> 
+    );
+    }
+  
+}
+
 
 export default AdCarousel;
