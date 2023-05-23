0ba106e448ce012de49b76bc619f7f7c112312e5
Anders Johansson <trumanders@gmail.com>
2023-05-23 12:56:19 +0200

    fixed-wrong-filename-in-ad.jsx

--- src/assets/components/Advertisement/Ad.jsx
+++ src/assets/components/Advertisement/Ad.jsx
@@ -1,69 +1,68 @@
 import React, { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import './Ad.css'
-import obey from '../../pictures/AdObey.png'
-import korv from '../../pictures/AdKorv.png'
-import food from '../../pictures/AdFood.png'
-import hamb from '../../pictures/adNailSoup.jpg'
-import admobile from '../../pictures/adNailSoupMobile.jpg'
+import "./Ad.css";
+import obey from "../../pictures/AdObey.png";
+import korv from "../../pictures/AdKorv.png";
+import food from "../../pictures/AdFood.png";
+import hamb from "../../pictures/adNailSoup.jpg";
+import admobile from "../../pictures/adNailSoupMobile.jpg";
 
 // window.innerWidth < 720 ? setadList(adMobilePhotos) : setadList(adPhotos)
- const adPhotos = [
-    { picture: hamb,
-      link: "https://www.byggmax.se/spik-och-skruv/spik"
-    },
-    { picture: hamb,
-      link: "https://www.biltema.se/bygg/fastelement/spik/"
-    }
- ]
+const adPhotos = [
+  { picture: hamb, link: "https://www.byggmax.se/spik-och-skruv/spik" },
+  { picture: hamb, link: "https://www.biltema.se/bygg/fastelement/spik/" },
+];
 
- const adMobilePhotos = [
-  { picture: admobile,
-    link: "https://www.byggmax.se/spik-och-skruv/spik"
-  },
-  { picture: admobile,
-    link: "https://www.biltema.se/bygg/fastelement/spik/"
-  }
- ]
+const adMobilePhotos = [
+  { picture: admobile, link: "https://www.byggmax.se/spik-och-skruv/spik" },
+  { picture: admobile, link: "https://www.biltema.se/bygg/fastelement/spik/" },
+];
 
 const AdCarousel = () => {
-  const[isMobile, setisMobile] = useState(window.innerWidth < 770)
-
+  const [isMobile, setisMobile] = useState(window.innerWidth < 770);
 
   const updateMedia = () => {
     setisMobile(window.innerWidth < 770);
   };
 
-useEffect(() => {
-  window.addEventListener("resize", updateMedia);
-  return () => window.removeEventListener("resize", updateMedia)
-}, [isMobile]);
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, [isMobile]);
 
-if(isMobile)
-{
-  return (
-    <Carousel style={{ margin: "25px" }}>
-      {adMobilePhotos.map((ad, index) => (
-        <Carousel.Item key={index}>
-          <a href={ad.link} target="_blank" rel="noopener noreferrer"><img src={ad.picture} alt={`Ad Photo ${index + 1}`} className="img" /></a>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-  );
-      }
-  else{
-    return(
-    <Carousel style={{ margin: "25px" }}>
-      {adPhotos.map((ad, index) => (
-        <Carousel.Item key={index}>
-          <a href={ad.link} target="_blank" rel="noopener noreferrer"><img src={ad.picture} alt={`Ad Photo ${index + 1}`} className="img" /></a>
-        </Carousel.Item>
-      ))}
-    </Carousel> 
+  if (isMobile) {
+    return (
+      <Carousel style={{ margin: "25px" }}>
+        {adMobilePhotos.map((ad, index) => (
+          <Carousel.Item key={index}>
+            <a href={ad.link} target="_blank" rel="noopener noreferrer">
+              <img
+                src={ad.picture}
+                alt={`Ad Photo ${index + 1}`}
+                className="img"
+              />
+            </a>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     );
-    }
-  
-}
-
+  } else {
+    return (
+      <Carousel style={{ margin: "25px" }}>
+        {adPhotos.map((ad, index) => (
+          <Carousel.Item key={index}>
+            <a href={ad.link} target="_blank" rel="noopener noreferrer">
+              <img
+                src={ad.picture}
+                alt={`Ad Photo ${index + 1}`}
+                className="img"
+              />
+            </a>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    );
+  }
+};
 
 export default AdCarousel;
